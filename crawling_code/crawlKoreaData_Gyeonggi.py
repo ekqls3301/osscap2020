f79b2d39a8931fb3ba4277d46a75c16df2b221d5
miyeong75 <pmy63096276@gmail.com>
2020-11-26 18:16:07 +0900

    updated_crawlKoreaData_Gyeonggi.py

--- crawling_code/crawlKoreaData_Gyeonggi.py
+++ crawling_code/crawlKoreaData_Gyeonggi.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from utils import write_data
-
-import pandas as pd
+from datetime import date, timedelta
 
 def get_data(url):
     html = requests.get(url).text
@@ -22,29 +21,38 @@ def parse_data(data, updated):
         confirmed = confirmed.replace(',','')
         confirmed = int(confirmed)
 
+
         confirmed_Gyeonggi.append({
             '지역이름': region,
-            '확진자수': confirmed
+            '확진자수': confirmed,
         })
+    confirmed_Gyeonggi.append({'업데이트날짜': updated})
 
     del confirmed_Gyeonggi[0]
     return confirmed_Gyeonggi
+
+
+
+
+today = date.today()
+yesterday = date.today() - timedelta(1)
+a = str(today)
+b = str(yesterday)
+
 
 def run():
     data, updated = get_data(
         "https://www.gg.go.kr/contents/contents.do?ciIdx=1150&menuId=2909")
     confirmed_region = parse_data(data, updated)
     # print(confirmed_region)
-    save_dir = 'koreaData(Gyeonggi).js'
-    crawler_name = 'crawlKoreaData(Gyeonggi).py'
-    var_name = 'koreaData(Gyeonggi)'
+    save_dir = 'koreaData_Gyeonggi_' + a + '.js'
+    crawler_name = 'crawlKoreaData_Gyeonggi.py'
+    var_name = 'koreaData_Gyeonggi'
 
     write_data(confirmed_region, save_dir, crawler_name, var_name)
 
 
 run()
-
-
 
 
 
