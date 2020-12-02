83d9bd65f881762c3d86f4b823ac452533e5f8a9
ekqls3301 <72439207+ekqls3301@users.noreply.github.com>
2020-12-02 17:20:40 +0900

    Update main_10.py

--- crawling_update(LED)/main_10.py
+++ crawling_update(LED)/main_10.py
@@ -388,6 +388,16 @@ def clear_array(array):
         for j in range(32):
             array[i][j] = 0
 
+def getdata(js_file):
+    with open (js_file,"r",encoding="utf-8") as f:
+        json_data = json.load(f)
+        text =''
+        for i in range(0,len(json_data)-1):
+            data_name = json_data[i]['지역이름']
+            data_num = json_data[i]['확진자수']
+            text = text + data_name + " : " + str(data_num) + "  "
+        return text
+  
 main_menu = 0
 menu = 1
 while(menu):
